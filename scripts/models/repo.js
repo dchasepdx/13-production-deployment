d3aef05663325025d9dac697a8fa38656468d49e
dchasepdx <dchasepdx@gmail.com>
2016-10-05 13:36:43 -0700

    refactored $.get into a $.ajax call

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -5,8 +5,10 @@
 
   reposObj.requestRepos = function(callback) {
     // TODO: refactor this request into an $.ajax call
-    $.get('/github/users/codefellows-portland-301d6/repos' +
-          '?per_page=10&sort=updated')
+    $.ajax('/github/users/codefellows-portland-301d6/repos' +
+          '?per_page=10&sort=updated', {
+            method: 'GET',
+          })
           .done(function(data) {
             reposObj.allRepos = data;
           }).done(callback);
